e78c6bff172e195451fe64469ebe728b13f6e27a
arjunaju123 <54721arjun@gmail.com>
2024-03-27 13:05:35 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Arjun S',
+    author_email= '54721arjun@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
